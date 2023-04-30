ab274eb94bdedd53840c291f7e7540c6f9684f79
MickaelThibeault <mickal.thibeault2022@campus-eni.fr>
2023-04-30 17:50:45 +0200

    Restitution pour la pre-prod

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -22,11 +22,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[Route('/sortie', name: 'sortie_')]
 

0c933de5f8fe02f9ae20f8c41ac36b7feeed9546
MickaelThibeault <mickal.thibeault2022@campus-eni.fr>
2023-05-01 20:26:52 +0200

    Ajout Gestion des campus et des villes/ ajout des contraintes sur les attributs des entités

--- src/Controller/CampusController.php
+++ src/Controller/CampusController.php
@@ -2,23 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Campus;
 use App\Repository\CampusRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[Route('/campus', name: 'campus_')]
+//#[Security('is_granted(\'ROLE_ADMINISTRATEUR\')')]
 class CampusController extends AbstractController
 {
 
-    #[Route('/', name: 'index')]
-    public function index(Request $request, CampusRepository $campusRepository): Response
+    #[Route('/index', name: 'index')]
+    public function index(Request $request, CampusRepository $campusRepository, EntityManagerInterface $entityManager): Response
     {
 
         $searchCampusForm = $this->createFormBuilder()
-            ->add('nomCampus', TextType::class,['label' => 'Le nom contient : ','required'=> false])
+            ->add('nomCampus', TextType::class,[
+                'label' => 'Le nom contient : ',
+                'required'=> false,
+                'constraints'=>[
+                    new NotBlank([
+                        'message' => 'Veuillez saisir une partie du nom du Campus'
+                ])
+            ]])
             ->getForm();
 
         $searchCampusForm->handleRequest($request);
@@ -32,19 +43,49 @@ class CampusController extends AbstractController
             $campus = $campusRepository->findAll();
         }
 
+        $campusForm = $this->createFormBuilder()
+            ->add('nom', TextType::class, [
+                'required'=>false,
+                'constraints'=>[
+                    new NotBlank([
+                        'message' => 'Veuillez saisir le nom du Campus'
+                    ])
+                ]
+            ])
+            ->getForm();
+
+        $campusForm->handleRequest($request);
+
+        if ($campusForm->isSubmitted() && $campusForm->isValid()) {
+            $campusAjout = new Campus();
+            $campusAjout->setNom($campusForm->get('nom')->getData());
+
+            $entityManager->persist($campusAjout);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('campus_index');
+        }
+
         return $this->render('campus/index.html.twig', [
             'searchCampusForm'=>$searchCampusForm->createView(),
-            'campus'=>$campus
+            'campus'=>$campus,
+            'campusForm'=>$campusForm->createView()
         ]);
     }
 
-    #[Route('/', name: 'update')]
+    #[Route('/update', name: 'update')]
     public function update($id) {
 
     }
 
-    #[Route('/', name: 'delete')]
-    public function delete($id) {
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete($id, CampusRepository $campusRepository, EntityManagerInterface $entityManager) {
 
+        $campus = $campusRepository->find($id);
+
+        $entityManager->remove($campus);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('campus_index');
     }
 }
